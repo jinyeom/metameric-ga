554651cfbf8f65a02496a9dd8505752688a8182f
wh1t3w01f <jinyeom95@gmail.com>
2016-10-04 01:31:47 -0500

    progress

--- mga.py
+++ mga.py
@@ -1,35 +1,36 @@
 # METAMERIC GENETIC ALGORITHM (MGA)
 #
-#   AUTHOR:     JINSEOK YEOM
-#   VERSION:    0.1
-#   SINCE:      10/03/2016
+# AUTHOR:     JINSEOK YEOM
+# VERSION:    0.1
+# SINCE:      10/03/2016
 #
 
 import numpy as np
 import numpy.random as npr
 
-class Metavariable:
-   
-    # initialize random design variables with given size.  
+# Metavariable
+class Metavar(object):
     def __init__(self, size):
         self.size = size
         self.dv = npr.rand(size)
 
     # calculate dissimilarity with other metavariable.
     def dissimilarity(self, m1):
-        assert (self.size == m1.size), "Unmatching sizes of metavariables"
+        assert (self.size == m1.size), "invalid argument metavariable"
+        return (abs()) / float(size)
 
-class Genome:
-
-    # initialize 
-    def __init__(self, length):
+# Variable-length Genome
+class Genome(object):
+    def __init__(self, mv_size, length):
         self.length = length
-        
+        self.genome = [Metavariable(mv_size) for x in range(length)]       
+ 
     def mutate(self):
         return
     
     def sm_recombination(self):
         return
 
-class MGA:
+# Metameric Genetic Algorithm
+class MGA(object):
 
